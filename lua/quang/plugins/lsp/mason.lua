4b165400d24fe696a9090c5de9b3f9630134e147
Quang Nguyễn <quangnguyen@Quangs-MacBook-Air-7.local>
2025-03-20 18:00:49 -0400

    added lsp configs

--- lua/quang/plugins/lsp/mason.lua
+++ lua/quang/plugins/lsp/mason.lua
@@ -25,30 +25,45 @@ return {
 		})
 
 		mason_lspconfig.setup({
-			-- list of servers for mason to install
+			-- list of servers for mason to install (LSP)
 			ensure_installed = {
+				-- Rust gigs --
+				"rust_analyzer",
+				-- Go familia --
+				"gopls",
+				-- My C familia --
+				"clangd",
+				-- Web familia --
 				"tsserver",
 				"html",
 				"cssls",
 				"tailwindcss",
 				"svelte",
-				"lua_ls",
 				"graphql",
 				"emmet_ls",
 				"prismals",
+				-- Lua for nvim --
+				"lua_ls",
+				-- Python --
 				"pyright",
+				-- Java --
+				"jdtls", -- Java LSP
 			},
+			automatic_installation = true,
 		})
 
 		mason_tool_installer.setup({
 			ensure_installed = {
+				-- Doesn't need clang-tidy since clangd is already installed or come with clangd
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
 				"isort", -- python formatter
 				"black", -- python formatter
 				"pylint", -- python linter
 				"eslint_d", -- js linter
+				"eslint",
 			},
+			automatic_installation = true,
 		})
 	end,
 }
